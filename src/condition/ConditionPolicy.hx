80839376bb1d46a0612711c335dffdc484af0701
P.J.Shand <pete@peteshand.net>
2018-12-26 10:19:32 +1100

    initial commit

---
+++ src/condition/ConditionPolicy.hx
@@ -0,0 +1,12 @@
+package condition;
+
+/**
+ * ...
+ * @author P.J.Shand
+ */
+@:enum abstract ConditionPolicy(Int) from Int to Int from UInt to UInt {
+	
+	public var AND = 0;
+	public var OR = 1;
+	public var SCENE = 2;
+}
---
+++ src/condition/IState.hx
@@ -0,0 +1,25 @@
+package condition;
+
+import notifier.Notifier;
+import notifier.Signal;
+
+/**
+ * ...
+ * @author P.J.Shand
+ */
+interface IState
+{
+	public var conditionPolicy:ConditionPolicy;
+	public var value(get, set):Null<Bool>;
+	public var onActive:Signal;
+	public var onInactive:Signal;
+	public function addURI(uri:String):Void;
+	public function addURIMask(uri:String):Void;
+	public function removeURI(uri:String):Void;
+	public function removeURIMask(uri:String):Void;
+	public function addCondition(notifier:Notifier<Dynamic>, value:Dynamic, operation:String = "==", subProp:String=null):Void;
+	public function removeCondition(notifier:Notifier<Dynamic>, value:Dynamic = null, operation:String = null):Void;
+	public function check(forceDispatch:Bool = false):Bool;
+	public function dispose():Void;
+	public function clone():State;
+}
