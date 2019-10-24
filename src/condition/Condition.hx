6da918a101f21f5c90bc385ad4516ca950f5d296
P.J.Shand <pete@peteshand.net>
2019-10-25 10:23:54 +1100

    update checkWithPolicy order

--- src/condition/Condition.hx
+++ src/condition/Condition.hx
@@ -1,18 +1,12 @@
 package condition;
 
-import haxe.macro.MacroStringTools;
 import notifier.Notifier;
 import signal.Signal;
 import haxe.extern.EitherType;
 import haxe.Timer;
-import utils.FunctionUtil;
-// import condition.modifier.DefaultModifier;
-// import condition.modifier.IModifier;
 import haxe.macro.Expr;
 import haxe.macro.Context;
-import Type as RunTimeType;
 import haxe.macro.Type.ClassType;
-import location.Location;
 
 // #if macro
 using haxe.macro.Tools;
@@ -27,39 +21,36 @@ using haxe.macro.Tools;
 @:access(condition.Condition.Case)
 class Condition extends Notifier<Bool> {
 	macro public static function make(expr:haxe.macro.Expr):haxe.macro.Expr {
-		// trace(expr.toString());
+		var exprStr:String = expr.toString();
+
 		var notifiers:Array<String> = [];
 		var wrapped:Bool = findNotifiers(expr, notifiers, 0);
-		var exprStr:String = expr.toString();
-		// trace("wrapped = " + wrapped);
+
 		if (!wrapped)
 			exprStr = 'function() return $exprStr';
 
 		// trace("notifiers = " + notifiers);
 		// trace(exprStr);
-
-		return Context.parse('untyped new Condition($notifiers, $exprStr)', Context.currentPos());
+		return untyped Context.parse('new Condition($notifiers, $exprStr)', Context.currentPos());
 	}
 
 	#if macro
 	public static function findNotifiers(e:haxe.macro.Expr, props:Array<String>, level:Int) {
 		if (e == null)
 			return false;
-		// trace("---");
+		//
+		//
+
+		// trace("\n\n");
 		// trace(e.expr);
+		var nextLevel:Int = level + 1;
 		switch (e.expr) {
 			case EField(e, field):
-				// trace("1 EField");
-
-				// trace(e.toString());
-
 				var type:haxe.macro.Type = Context.typeof(Context.parse(e.toString(), Context.currentPos()));
 				if (isNotifier(type)) {
 					props.push(e.toString());
 				}
-			// findNotifiers(e, props, level + 1);
 			case EConst(CIdent(s)):
-				// trace("2 EConst");
 				switch (s) {
 					case 'null' | 'true' | 'false':
 					default:
@@ -69,56 +60,50 @@ class Condition extends Notifier<Bool> {
 						//	props.push(s);
 						// }
 
-						var type:haxe.macro.Type = Context.typeof(Context.parse(s, Context.currentPos()));
-						if (isNotifier(type)) {
-							props.push(s);
+						try {
+							var type:haxe.macro.Type = Context.typeof(Context.parse(s, Context.currentPos()));
+							if (isNotifier(type)) {
+								props.push(s);
+							}
+						} catch (e:Dynamic) {
+							// trace(e);
 						}
 				}
 			case EConst(CInt(s) | CFloat(s) | CString(s)):
-			// ("3 EConst");
 			// ignore
 			case EBinop(_, e1, e2):
-				// trace("4 EBinop");
-				var nextLevel:Int = level + 1;
 				findNotifiers(e1, props, nextLevel);
 				findNotifiers(e2, props, nextLevel);
 			case EFunction(name, f):
-				// trace("5 EFunction");
-				findNotifiers(f.expr, props, level + 1);
-				// trace("5 EFunction B");
+				findNotifiers(f.expr, props, nextLevel);
 				if (level == 0)
 					return true;
 			case EReturn(e):
-				// trace("6 EReturn");
-				findNotifiers(e, props, level++);
+				findNotifiers(e, props, nextLevel);
 			case ECall(e, params):
-				// trace("7 ECall");
-				findNotifiers(e, props, level++);
+				findNotifiers(e, props, nextLevel);
+				for (expr in params) {
+					findNotifiers(expr, props, nextLevel);
+				}
 			case EMeta(s, e):
-				// trace("8 EMeta");
-				findNotifiers(e, props, level++);
+				findNotifiers(e, props, nextLevel);
 			case EIf(econd, eif, eelse):
-				// trace("9 EIf");
-				findNotifiers(econd, props, level);
-				findNotifiers(eif, props, level);
-				findNotifiers(eelse, props, level);
-				level++;
+				findNotifiers(econd, props, nextLevel);
+				findNotifiers(eif, props, nextLevel);
+				findNotifiers(eelse, props, nextLevel);
 			case EBlock(exprs):
-				// trace("10 EBlock");
-				// Current not parsing EBlock
-				// findNotifiers(expr, props, level++);
 				for (expr in exprs) {
 					// trace("1 expr: " + expr);
-					findNotifiers(expr, props, level);
+					findNotifiers(expr, props, nextLevel);
 				}
-
 			case EUnop(op, postFix, e):
-				findNotifiers(e, props, level);
+				findNotifiers(e, props, nextLevel);
 			case EVars(vars):
 				for (_var in vars) {
-					findNotifiers(_var.expr, props, level);
+					findNotifiers(_var.expr, props, nextLevel);
 				}
-
+			case EParenthesis(e):
+				findNotifiers(e, props, nextLevel);
 			case _:
 				trace("unhandled: " + e.expr);
 		}
@@ -126,38 +111,17 @@ class Condition extends Notifier<Bool> {
 	}
 
 	static function isNotifier(type:haxe.macro.Type) {
-		// trace('type = ' + type);
 		switch (type) {
-			case TMono(t): // t:Ref<Null<Type>>
-			// trace("1 TMono");
-			case TEnum(t, params): // t:Ref<EnumType>, params:Array<Type>
-			// trace("2 TEnum");
-			case TInst(t, params): // t:Ref<ClassType>, params:Array<Type>
+			case TInst(t, params):
 				var classType:ClassType = type.getClass();
-				// var module:String = classType.module;
-				// if (module == 'notifier.Notifier') {
-				//	return true;
-				// }
 				return inherents(classType);
-			case TType(t, params): // t:Ref<DefType>, params:Array<Type>
-			// trace("3 TType");
-			case TFun(args, ret): // args:Array<{t:Type, opt:Bool, name:String}>, ret:Type
-			// trace("4 TFun");
-			case TAnonymous(a): // a:Ref<AnonType>
-			// trace("5 TAnonymous");
-			case TDynamic(t): // t:Null<Type>
-			// trace("6 TDynamic");
-			case TLazy(f): // f:Void ‑> Type
-			// trace("7 TLazy");
-			case TAbstract(t, params): // t:Ref<AbstractType>, params:Array<Type>
-				// trace("8 TAbstract");
+			case _:
+				// ignore
 		}
 		return false;
 	}
 
 	static function inherents(classType:ClassType):Bool {
-		// trace("classType.module = " + classType.module);
-
 		if (classType.module == 'notifier.Notifier') {
 			return true;
 		} else if (classType.superClass != null) {
@@ -191,13 +155,15 @@ class Condition extends Notifier<Bool> {
 		});
 
 		if (notifiers != null && testFunc != null) {
-			addFunc(notifiers, testFunc);
+			for (notifier in notifiers) {
+				addFunc(notifier, testFunc);
+			}
 		}
 
 		check();
 	}
 
-	function addFunc(notifier:NotifierOrArray, checkFunction:haxe.Constraints.Function /*, modifier:IModifier = null*/):Condition {
+	function addFunc(notifier:Notifier<Dynamic>, checkFunction:haxe.Constraints.Function /*, modifier:IModifier = null*/):Condition {
 		_add(new Case(notifier, checkFunction /*, modifier*/));
 		return this;
 	}
@@ -261,9 +227,11 @@ class Condition extends Notifier<Bool> {
 
 	function checkWithPolicy(cases:Array<Case>) {
 		var bitOperator = BitOperator.AND;
-		var _value:Bool = true;
 		for (i in 0...cases.length) {
 			cases[i].check();
+		}
+		var _value:Bool = true;
+		for (i in 0...cases.length) {
 			var caseValue:Bool = cases[i].value;
 			if (bitOperator == BitOperator.AND)
 				_value = _value && caseValue;
@@ -316,99 +284,55 @@ class Condition extends Notifier<Bool> {
 }
 
 class Case extends Notifier<Bool> {
-	// static var defaultModifier = new DefaultModifier();
 	public var bitOperator = BitOperator.AND;
-	public var notifiers:Array<Notifier<Dynamic>>;
-	public var conditions:Array<Condition> = [];
+	public var innerCheckFunction:haxe.Constraints.Function;
 	public var checkFunction:haxe.Constraints.Function;
 
-	// public var modifier:IModifier;
-	var notifier:NotifierOrArray;
-	var isArray:Bool;
-	var values:Array<Dynamic> = [];
+	var notifier:Notifier<Dynamic>;
+	var condition:Condition;
 
 	public var debug:String;
 
-	public function new(notifier:NotifierOrArray, checkFunction:haxe.Constraints.Function /*, _modifier:IModifier = null*/) {
+	public function new(notifier:Notifier<Dynamic>, checkFunction:haxe.Constraints.Function /*, _modifier:IModifier = null*/) {
 		super();
 
 		this.notifier = notifier;
-		this.checkFunction = checkFunction;
-		/*if (_modifier != null) {
-				modifier = _modifier;
-			} else {
-				modifier = defaultModifier;
-		}*/
+		this.innerCheckFunction = checkFunction;
 
-		if (Std.is(notifier, Array)) {
-			this.notifiers = notifier;
-			isArray = true;
-		} else {
-			notifiers = [cast(notifier, Notifier<Dynamic>)];
-			isArray = false;
+		if (Std.is(notifier, Condition)) {
+			condition = untyped notifier;
 		}
 
-		for (i in 0...notifiers.length) {
-			if (Std.is(notifiers[i], Condition)) {
-				conditions.push(untyped notifiers[i]);
+		this.checkFunction = () -> {
+			if (condition != null) {
+				condition.check();
 			}
-			values.push(notifiers[i].value);
-			notifiers[i].add(() -> {
-				check();
-			}).priority(1000);
-		}
+			innerCheckFunction();
+		};
+
+		notifier.add(() -> {
+			check();
+		}).priority(1000);
 
 		check();
 	}
 
-	public function match(value:NotifierOrArray, checkFunction:haxe.Constraints.Function):Bool {
+	public function match(value:Notifier<Dynamic>, checkFunction:haxe.Constraints.Function):Bool {
 		if (value != null) {
-			if (isArray) {
-				var _notifiers:Array<Notifier<Dynamic>> = value;
-				var matchCount:Int = 0;
-				for (i in 0..._notifiers.length) {
-					for (j in 0...notifiers.length) {
-						if (_notifiers[i] == notifiers[j])
-							matchCount++;
-					}
-				}
-				if (matchCount != notifiers.length)
-					return false;
-			} else {
-				if (notifiers[0] != value)
-					return false;
-			}
+			if (notifier != value)
+				return false;
 		}
-		if (this.checkFunction != checkFunction && checkFunction != null)
+		if (this.innerCheckFunction != checkFunction && checkFunction != null)
 			return false;
 		return true;
 	}
 
 	public function check(forceDispatch:Bool = false):Void {
-		for (i in 0...conditions.length) {
-			conditions[i].check();
-		}
 		this.value = checkFunction();
-		/*if (debug != null) {
-			trace("check: " + debug + " - " + this.value + " - " + Location.instance.uri);
-		}*/
-		/*
-			for (i in 0...notifiers.length) {
-				trace(notifiers[i].value);
-				values[i] = notifiers[i].value;
-			}
-			modifier.setValue(FunctionUtil.dispatch(checkFunction, values), forceDispatch, onModChange); */
 	}
 
-	/*function onModChange(value:Bool, forceDispatch:Bool) {
-		this.value = modifier.value;
-		trace("onModChange: " + debug + " - " + this.value);
-		if (forceDispatch)
-			this.dispatch();
-	}*/
 	public function clone():Case {
-		// trace(notifier == null);
-		var newCase = new Case(notifier, checkFunction /*, modifier*/);
+		var newCase = new Case(notifier, innerCheckFunction /*, modifier*/);
 		newCase.bitOperator = bitOperator;
 		return newCase;
 	}
@@ -436,5 +360,3 @@ class SignalA extends Signal {
 		return this;
 	}
 }
-
-typedef NotifierOrArray = EitherType<Notifier<Dynamic>, Array<Notifier<Dynamic>>>;
